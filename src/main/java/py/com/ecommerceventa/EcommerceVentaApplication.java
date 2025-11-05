d1d87bf27006aeabc6908bf7acfddd5fb55260d2
MatiasWinter <matiaswinter014@gmail.com>
2025-11-05 09:08:39 -0300

    Inicializa proyecto de ventas con Spring Boot

---
+++ src/main/java/py/com/ecommerceventa/EcommerceVentaApplication.java
@@ -0,0 +1,13 @@
+package py.com.ecommerceventa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceVentaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceVentaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/py/com/ecommerceventa/EcommerceVentaApplicationTests.java
@@ -0,0 +1,13 @@
+package py.com.ecommerceventa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceVentaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
